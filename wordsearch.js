19fc9ec5f0e4c99100066b3b2e12df32e41aa1b1
takuyadev <takuya.k.toyokawa@protonmail.com>
2023-03-15 20:22:43 -0700

    finish pair :)

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,28 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+    if (l.split("").reverse().join("").includes(word)) return true;
+  }
 
-module.exports = wordSearch
+  const verticalArray = [];
+
+  for (let i = 0; i < letters[0].length; i++) {
+    let tempArray = [];
+    for (let j = 0; j < letters.length; j++) {
+      tempArray.push(letters[j][i]);
+    }
+    verticalArray.push(tempArray);
+  }
+
+  const verticalJoin = verticalArray.map((arr) => arr.join(""));
+
+  for (l of verticalJoin) {
+    if (l.includes(word)) return true;
+    if (l.split("").reverse().join("").includes(word)) return true;
+  }
+
+  return false;
+};
+
+module.exports = wordSearch;
